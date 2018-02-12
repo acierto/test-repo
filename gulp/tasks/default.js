2a38ec785ec33a1dd4fa182fc4de192a696eba39
Bogdan Nechyporenko <bnechyporenko@xebialabs.com>
2018-02-12 20:18:15 +0100

    bumped up

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,18 +1,13 @@
-import util from 'util';
 import gulp from 'gulp';
 import {DefaultReleaseRegistry, release} from 'gulp-release-it';
 
-util.inherits(CustomGulpReleaseRegistry, DefaultReleaseRegistry);
-
-function CustomGulpReleaseRegistry() {
-    DefaultReleaseRegistry.call(this);
+class CustomGulpReleaseRegistry extends DefaultReleaseRegistry {
+    init(takerInst) {
+        takerInst.task('pre-tag-and-push', (cb) => {
+            console.log('i can override default task definition');
+            cb();
+        });
+    }
 }
-
-CustomGulpReleaseRegistry.prototype.init = function (takerInst) {
-    takerInst.task('pre-tag-and-push', function (cb) {
-        console.log('i can override default task definition');
-        cb();
-    });
-};
 
 release(gulp, [new CustomGulpReleaseRegistry()]);
